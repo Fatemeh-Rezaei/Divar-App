5e75be7450cf94c7b51be41e4c70b214f7c96c28
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-06-15 16:09:26 +0430

    config axios

---
+++ src/configs/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
